1caf1fa95db1f72c44639fd1a0838c380ccad388
Rezwan Bin Nehal <rezwanbinnehal011@gmail.com>
2023-04-04 02:29:46 +0600

    Practice Day 01

--- Practice Day 01/Problem_02.c
+++ Practice Day 01/Problem_02.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int a=10,b=3;
+    int a,b;
+    char pls = '+',min='-',gun='*',vag='/';
+
+    scanf("%d %d",&a,&b);
+       
     int sum=a+b;
     int sub=a-b;
     int mul=a*b;
     float div=a*1.0/b;
-    printf("%0.2f",div);
+    printf("%d%c%d=%d\n",a,pls,b,sum);
+    printf("%d%c%d=%d\n",a,min,b,sub);
+    printf("%d%c%d=%d\n",a,gun,b,mul);
+    printf("%d%c%d=%0.2f",a,vag,b,div);
     return 0;
 }
